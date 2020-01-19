65c4d1475ad48a1b3166b771364637ac1ef1e154
MadeleineWallner <madeleine.wallner@gmail.com>
2020-01-19 13:07:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,7 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
 
 //global variables, one to store the list of students and one to store the maximum number of students per page
 const students = document.getElementsByClassName('student-item cf');
 const studentsPerPage = 10; 
-console.log(students);
 
 
 //creating the search bar and search button
@@ -57,42 +19,49 @@ searchDiv.appendChild(button);
 
 
 
+
 //function to filter out which students to show and which to hide
-const studentList = document.querySelectorAll('h3');
 
-
-
-const searchFunction = (names, input) => {
-  for (i = 0; i < names.length; i++){
-   if (names[i].textContent.toLowerCase().includes(input.value.toLowerCase())){
-      names[i].style.display = 'block';
-      console.log(names[i]);
+const searchFunction = (names, input) => { 
+   let matches = [];
+   let paginationLinks = document.getElementsByClassName('pagination')[0];
+   if (paginationLinks){
+      paginationLinks.remove();
    }
-   
-}
+  for (let i = 0; i < names.length; i++){
+   if (names[i].textContent.toLowerCase().includes(input.value.toLowerCase())){
+      matches.push(names[i]);
+
+   } else { names[i].style.display = 'none'
+   } 
+  }
+  showPage(matches, 1);
+  pageLinks(matches)
+  
+    //if there's no matches, print out 'No results'
+    if (matches.length === 0){
+      const noMatches = document.createElement('p');
+      noMatches.innerHTML = 'No results';
+      const studentsUL = document.getElementsByClassName('student-list')[0];
+      studentsUL.appendChild(noMatches);
+   }
 };
 
 
+//keyup eventlistener to show results while the user is typing
+searchBar.addEventListener('keyup', (e) => {
+   searchFunction(students, searchBar);
+});
+
+//click eventlistener to show search results when the user clicks the search button
 button.addEventListener('click', (e) => {
    e.preventDefault();
-   searchFunction(studentList, searchBar);
-   
-
+   searchFunction(students, searchBar);
 });
 
 
 
-
-
-
-
-
-
-
-
-// function to show no more than 10 students per page and hide the rest. 
- 
- 
+// function to show no more than 10 students per page and hide the rest.  
 const showPage = (list, page) => {
    const startIndex = (page * studentsPerPage) - studentsPerPage;
    const endIndex =  page * studentsPerPage;
@@ -112,8 +81,9 @@ for (let i = 0; i < list.length; i++){
 //Creating the pagination div and giving it the class name 'pagination', appending it to the div with the 'page' class name
 //Creating the ul element and appending it to the pagination Div 
 
-const appendPageLinks = (list) => {
-const pages = Math.ceil(students.length/studentsPerPage);
+const pageLinks = (list) => {
+let studentCount = list.length;
+const pages = Math.ceil(studentCount/studentsPerPage);
 const paginationDIV = document.createElement('div');
 paginationDIV.className = 'pagination';
 let page = document.getElementsByClassName('page')[0];
@@ -146,10 +116,6 @@ for (let i = 1; i <= pages; i++){
    }
 }
 
-
-//calling the 2 functions
+//calling the functions
 showPage(students, 1);
-appendPageLinks(students);
-
-
-
+pageLinks(students);
